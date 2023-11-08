6df8e9bd255ad13dd88e18448030644c67fe85be
Famba Abdulnassiir Siraji <57795345+FambaAbdulnassiirSiraji@users.noreply.github.com>
2023-11-08 19:09:23 +0300

    add next js 14 boiler plate

---
+++ app/page.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <main className='flex flex-col align-middle'>
+      <h1>Movie Library still in Developement...</h1>
+      <h3>Coming soon</h3>
+    </main>
+  )
+}
